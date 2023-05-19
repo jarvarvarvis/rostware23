8e57c931da25a2d5077b61ea8d748e8c98e01bb0
jarvarvarvis <jarvistrigo@gmail.com>
2023-05-19 15:01:30 +0200

    Move rostware23_lib related benchmarks into the folder

--- rostware23_lib/benches/current_team.rs
+++ rostware23_lib/benches/current_team.rs
@@ -4,9 +4,9 @@ extern crate criterion;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rostware23_lib::game::state_generator::*;
 
-pub fn possible_moves_from_any_state_benchmark(c: &mut Criterion) {
+pub fn current_team_of_any_state_benchmark(c: &mut Criterion) {
     c.bench_function("current team", |b| b.iter(|| black_box(create_any().current_team())));
 }
 
-criterion_group!(benches, possible_moves_from_any_state_benchmark);
+criterion_group!(benches, current_team_of_any_state_benchmark);
 criterion_main!(benches);
