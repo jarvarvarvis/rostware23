cc24bc3ea47059488b4ea1281e786bab5167c344
jarvarvarvis <jarvistrigo@gmail.com>
2023-05-19 15:15:20 +0200

    More specific possible move benchmarks

--- rostware23_lib/benches/possible_moves.rs
+++ rostware23_lib/benches/possible_moves.rs
@@ -3,9 +3,12 @@ extern crate criterion;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rostware23_lib::game::state_generator::*;
+use rostware23_lib::game::moves::Move;
 
 pub fn possible_moves_from_any_state_benchmark(c: &mut Criterion) {
-    c.bench_function("get possible moves", |b| b.iter(|| black_box(create_any().possible_moves())));
+    c.bench_function("get possible moves", |b| b.iter(|| black_box(
+                create_any().possible_moves().collect::<Vec<Move>>()
+    )));
 }
 
 criterion_group!(benches, possible_moves_from_any_state_benchmark);
