ba2d7e7353c1d9e480ede20b5d1b386ca9d4c6a9
jarvarvarvis <jarvistrigo@gmail.com>
2023-05-19 23:01:40 +0200

    Minor PVS prototype refactorings, also implement MoveGetter for it

--- src/logic/pvs_getter.rs
+++ src/logic/pvs_getter.rs
@@ -11,25 +11,34 @@ struct PVSResult {
     rating: i32
 }
 
-pub struct PVSMoveGetter {
-}
+pub struct PVSMoveGetter;
 
 impl PVSMoveGetter {
-    fn pvs(game_state: State) -> PVSResult {
+    pub fn new() -> Self {
+        Self
+    }
+
+    fn pvs(game_state: State) -> anyhow::Result<PVSResult> {
         let mut best_move = Move::Place(Coordinate::new(17, 9));
         let mut best_score = i32::min_value();
         let mut possible_moves = game_state.possible_moves();
         for current_move in possible_moves {
-            let current_score: i32 = game_state.board.get(current_move.get_to()).unwrap().get_fish_count().unwrap() as i32;
+            let current_score: i32 = game_state.board.get(current_move.get_to())?.get_fish_count()? as i32;
             if current_score > best_score {
                 best_move = current_move.clone();
                 best_score = current_score;
             }
         }
-        PVSResult {
+        Ok(PVSResult {
             best_move: best_move,
             rating: best_score
-        }
+        })
+    }
+}
+
+impl MoveGetter for PVSMoveGetter {
+    fn get_move(&self, state: &State) -> anyhow::Result<Move> {
+        Self::pvs(state.clone()).map(|result| result.best_move)
     }
 }
 
@@ -50,7 +59,7 @@ mod tests {
         board.set(Coordinate::new(14, 0), FieldState::Fish(1)).unwrap();
         let game_state = State::from_initial_board_with_start_team_one(board);
         let expected_move = Move::Normal{from: moving_penguin_coord, to: expected_target};
-        let result_got: PVSResult = PVSMoveGetter::pvs(game_state);
+        let result_got: PVSResult = PVSMoveGetter::pvs(game_state).unwrap();
         assert_eq!(expected_move, result_got.best_move);
     }
 
@@ -68,7 +77,7 @@ mod tests {
         board.set(Coordinate::new(14, 2), FieldState::Fish(1)).unwrap();
         let game_state = State::from_initial_board_with_start_team_one(board);
         let expected_move = Move::Normal{from: moving_penguin_coord, to: expected_target};
-        let result_got: PVSResult = PVSMoveGetter::pvs(game_state);
+        let result_got: PVSResult = PVSMoveGetter::pvs(game_state).unwrap();
         assert_eq!(expected_move, result_got.best_move);
     }
 }
