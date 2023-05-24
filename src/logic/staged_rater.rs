5c95de6041244ad90ea8db57433d47d1b86af858
jarvarvarvis <jarvistrigo@gmail.com>
2023-05-24 15:45:18 +0200

    Add tests for StagedRater

--- src/logic/staged_rater.rs
+++ src/logic/staged_rater.rs
@@ -35,3 +35,109 @@ impl<
         MID_MULTIPLIER * Heuristic::rate(state)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rostware23_lib::game::board::Board;
+    use rostware23_lib::game::common::Coordinate;
+    use rostware23_lib::game::moves::Move;
+    use rostware23_lib::xml::common::Team;
+    use rostware23_lib::xml::state::FieldState;
+
+    use super::*;
+
+    struct TestRater;
+
+    impl Rater for TestRater {
+        fn rate(_: &State) -> i32 {
+            1
+        }
+    }
+
+    type TestStagedRater = StagedRater::<2, 16, 91, TestRater>;
+
+    #[test]
+    fn staged_rater_in_early_game_with_test_rater_returns_expected_rating() {
+        let board = Board::empty();
+        let state = State::from_initial_board_with_start_team_one(board);
+        assert_eq!(2, TestStagedRater::rate(&state));
+    }
+
+    #[test]
+    fn staged_rater_in_mid_game_with_test_rater_returns_expected_rating() {
+        let mut board = Board::empty();
+        board
+            .set(Coordinate::new(0, 0), FieldState::Fish(2))
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(2, 0)), Team::One)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(4, 0)), Team::One)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(6, 0)), Team::One)
+            .unwrap();
+        board
+            .set(Coordinate::new(8, 0), FieldState::Fish(2))
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(12, 0)), Team::One)
+            .unwrap();
+
+        board
+            .perform_move(Move::Place(Coordinate::new(1, 1)), Team::Two)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(3, 1)), Team::Two)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(5, 1)), Team::Two)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(7, 1)), Team::Two)
+            .unwrap();
+        let mut state = State::from_initial_board_with_start_team_one(board);
+        state.turn = 8;
+        assert_eq!(16, TestStagedRater::rate(&state));
+    }
+
+    #[test]
+    fn staged_rater_in_end_game_with_test_rater_returns_expected_rating() {
+        let mut board = Board::empty();
+        board
+            .set(Coordinate::new(0, 0), FieldState::Fish(2))
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(2, 0)), Team::One)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(4, 0)), Team::One)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(6, 0)), Team::One)
+            .unwrap();
+        board
+            .set(Coordinate::new(8, 0), FieldState::Fish(2))
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(12, 0)), Team::One)
+            .unwrap();
+
+        board
+            .perform_move(Move::Place(Coordinate::new(2, 4)), Team::Two)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(4, 4)), Team::Two)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(6, 4)), Team::Two)
+            .unwrap();
+        board
+            .perform_move(Move::Place(Coordinate::new(8, 4)), Team::Two)
+            .unwrap();
+        let mut state = State::from_initial_board_with_start_team_one(board);
+        state.turn = 26;
+        assert_eq!(91, TestStagedRater::rate(&state));
+    }
+}
