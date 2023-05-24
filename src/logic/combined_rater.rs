fae3650a9864dba9786cb566c2744304341687de
jarvarvarvis <jarvistrigo@gmail.com>
2023-05-24 15:46:09 +0200

    Change weight of FishDifferenceRater in the end game to 50

--- src/logic/combined_rater.rs
+++ src/logic/combined_rater.rs
@@ -5,12 +5,13 @@ use super::potential_fish_rater::PotentialFishRater;
 use super::reachable_fish_rater::ReachableFishRater;
 use super::quadrant_occupation_rater::QuadrantOccupationRater;
 use super::penguin_cutoff_rater::PenguinCutOffRater;
+use super::staged_rater::StagedRater;
 
 pub struct CombinedRater {}
 
 impl Rater for CombinedRater {
     fn rate(state: &State) -> i32 {
-        11 * FishDifferenceRater::rate(state) +
+        StagedRater::<0, 11, 50, FishDifferenceRater>::rate(state) +
             2 * PotentialFishRater::rate(state) +
             2 * PenguinCutOffRater::rate(state) +
             5 * ReachableFishRater::rate(state) +
